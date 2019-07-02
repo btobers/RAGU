f4040c97e8eb45f9f55e1ce748ff36fcf8b94ea3
colonel-toby <tober@lpl.arizona.edu>
2019-07-02 07:12:45 -0700

    expand canvas when window size changes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-'''NOSEpick - currently in development stages
+'''
+NOSEpick - currently in development stages
 created by: Brandon S. Tober and Michael S. Christoffersen
 date: 25JUN19
+
 dependencies in requirements.txt
 '''
 
@@ -19,116 +21,102 @@ from tkinter import Button, Frame, messagebox, Canvas, filedialog
 ### USER SPECIFIED VARS ###
 in_path = '/home/anomalocaris/Desktop/tmp/20180819-215243.mat'
 
-
 ### CODE ###
 name = in_path.split('/')[-1].rstrip('.mat')
 
-amp, dist, dt = run.ingest(in_path)
-amp = np.transpose(amp)
+class NosepickGUI(tk.Tk):
+    def __init__(self, master):
+        self.master = master
+        master.title("NOSEpick")
+        self.setup()
 
+    def setup(self):
+        # Frames for data display and UI
+        self.controls = Frame(self.master)
+        self.controls.pack(side="top")
+        self.display = Frame(self.master)
+        self.display.pack(side="bottom", fill="both", expand=1)
+        # Blank data canvas
+        self.fig = mpl.figure.Figure()
+        self.ax = self.fig.add_subplot(111)
+        self.dataCanvas = FigureCanvasTkAgg(self.fig, self.master)
+        self.dataCanvas.get_tk_widget().pack(in_=self.display, side="bottom", fill="both", expand=1)
+        self.dataCanvas.draw()
+        # Button for help message
+        self.instButton = Button(self.master, text = "Instructions", command = self.insMsg)
+        self.instButton.pack(in_=self.controls, side="left")
+        # Button for loading data
+        self.loadButton = Button(self.master, text = "Load", command = self.load)
+        self.loadButton.pack(in_=self.controls, side="left")
+        # Button for exit
+        self.exitButton = Button(text = "Exit", fg = "red", command = self.close_window)
+        self.exitButton.pack(in_=self.controls, side="left")
+        # Empty fields for pick
+        self.xln = []
+        self.yln = []
+        self.pick, = self.ax.plot([],[],'r')  # empty line
+        # Register click and key events
+        self.keye = self.fig.canvas.mpl_connect('key_press_event', self.onkey)
+        self.clicke = self.fig.canvas.mpl_connect('button_press_event', self.addseg)
 
-# run.rgram(amp, dist, dt, name)
+    def load(self):
+        # Can be made fancier in the future
+        igst = ingester.ingester("h5py")
+        self.fname = filedialog.askopenfilename(initialdir = "./",title = "Select file",filetypes = (("mat files","*.mat"),("all files","*.*")))
+        self.data = igst.read(self.fname)
+        self.matplotCanvas()
 
-class Root(tk.Tk):
-
-    def __init__(self):
-        super(Root, self).__init__()
-        self.title("NOSEpick")
-        # self.minsize(800, 800)
-        self.button()
-        self.matplotCanvas(amp, dist, dt)
-
-    def matplotCanvas(self, amp, dist, dt):
-
+    def matplotCanvas(self):
         # create matplotlib figure and use imshow to display radargram
-        f = Figure()
-        a = f.add_subplot(111)
-        a.imshow(np.log(np.power(amp,2)), cmap='gray', aspect='auto', extent=[dist[0], dist[-1], amp.shape[0] * dt * 1e6, 0])
-        a.set_title(name)
-        a.set(xlabel = 'along-track distance [km]', ylabel = 'two-way travel time [microsec.]')
-
-        # create the canvas widget
-        canvas = FigureCanvasTkAgg(f, self)
-        canvas.draw()
-        canvas.get_tk_widget().pack(side="top", fill="both", expand=1)
-
-        # create exit button to close window
-        button = tk.Button(text = "Exit", fg = "red", command = self.close_window)
-        button.pack(side="right")
-
+        self.ax.imshow(np.log(np.power(self.data['amp'],2)), cmap='gray', aspect='auto', extent=[self.data['dist'][0], self.data['dist'][-1], self.data['amp'].shape[0] * self.data['dt'] * 1e6, 0])
+        self.ax.set_title(name)
+        self.ax.set(xlabel = 'along-track distance [km]', ylabel = 'two-way travel time [microsec.]')
         # add matplotlib figure nav toolbar
-        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar = NavigationToolbar2Tk(self.dataCanvas, self.master)
         toolbar.update()
-        canvas._tkcanvas.pack(side="top", fill="both", expand=1)
+        self.dataCanvas._tkcanvas.pack()
+        self.dataCanvas.draw()
 
-    def button(self):
+    def addseg(self, event):
+        # add line segments with user input
+        if (event.inaxes != self.ax):
+            return
+        self.xln.append(event.xdata)
+        self.yln.append(event.ydata)
+        self.pick.set_data(self.xln, self.yln)
+        self.fig.canvas.draw()
 
-        # create instructions button
-        self.btn = ttk.Button(self, text = "Instructions", command = self.mssgBox)
-        self.btn.pack(side="top")
+    def onkey(self, event):
+        # on-key commands
+        if event.key =='c':
+            # clear the drawing of line segments
+            if len(self.xln) and len(self.yln) > 0:
+                del self.xln[:]
+                del self.yln[:]
+                self.pick.set_data(self.xln, self.yln)
+                self.fig.canvas.draw()
     
-    def mssgBox(self):
-
+        elif event.key =='backspace':
+            # remove last segment
+            if len(self.xln) and len(self.yln) > 0:
+                del self.xln[-1:]
+                del self.yln[-1:]
+                self.pick.set_data(self.xln, self.yln)
+                self.fig.canvas.draw()
+    
+    def insMsg(self):
         # instructions button message box
-        msg.showinfo("NOSEpick Instructions", """Nearly Optimal Subsurface Extractor:
+        messagebox.showinfo("NOSEpick Instructions",
+        """Nearly Optimal Subsurface Extractor:
         \n\n\u2022Click along reflector surface
         \n\u2022<spacebar> to remove the last pick
         \n\u2022<c> to remove all picks""")
 
     def close_window(self):
-
         # destroy canvas upon Exit button click
-        self.destroy()
+        self.master.destroy()
 
 
-root = Root()
+root = tk.Tk()
+gui = NosepickGUI(root)
 root.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# root = tk.Tk()
-# app = NOSEpick(master=root)
-# app.mainloop()
-
-# def close_window ():
-#     root.destroy()
-
-
-# root = tk.Tk()
-# root.wm_title("NOSEpick")
-# # root_panel = tk.Frame(root)
-
-# f = Figure()
-# a = f.add_subplot(111)
-
-# a.imshow(np.log(np.power(amp,2)), cmap='gray', aspect='auto', extent=[dist[0], dist[-1], amp.shape[0] * dt * 1e6, 0])
-# a.set_title(name)
-# a.set(xlabel = 'along-track distance [km]', ylabel = 'two-way travel time [microsec.]')
-
-# canvas = FigureCanvasTkAgg(f, master=root)
-# canvas.draw()
-# canvas.get_tk_widget().pack(side="top", fill="both", expand=1)
-# canvas._tkcanvas.pack(side="top", fill="both", expand=1)
-
-# button = tk.Button(text = "Exit", fg = "red", command = close_window)
-# button.pack(side="right")
-
-# toolbar = NavigationToolbar2Tk(canvas,root)
-# canvas._tkcanvas.pack(side="top", fill="both", expand=1)
-
-
-
-# root.mainloop()
