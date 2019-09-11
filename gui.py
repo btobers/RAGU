f7f61b5e9f35e2298619279722094b92ea853f53
colonel-toby <tober@lpl.arizona.edu>
2019-09-10 17:11:27 -0700

    reorg

--- gui.py
+++ gui.py
@@ -7,10 +7,10 @@ environment requirements in nose_env.yml
 """
 
 ### IMPORTS ###
-import ingester
-import utils
-import imPick
-import basemap
+# import ingester
+# import utils
+# import imPick
+# import basemap
 from tools import *
 import os, sys, scipy
 import numpy as np
@@ -20,14 +20,14 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 import gdal, osr
 import tkinter as tk
-from tkinter import Button, Frame, messagebox, Canvas, filedialog
+from tkinter import Button, Frame, messagebox, Canvas, filedialog, Menu
 
 
 class gui(tk.Tk):
     def __init__(self, master):
         self.master = master
         # self.master.title("NOSEpick")
-        self.master.protocol("WM_DELETE_WINDOW", imPick.imPick.close_window)
+        # self.master.protocol("WM_DELETE_WINDOW", imPick.imPick.close_window)
         self.setup()
 
 
@@ -51,64 +51,73 @@ class gui(tk.Tk):
         self.reset_ax = self.fig.add_axes([0.935, 0.11, 0.04, 0.03])
         # self.dataCanvas.get_tk_widget().pack(in_=self.display, side="bottom", fill="both", expand=1)
         # self.dataCanvas.draw()
+
+
+        # file menu
+        self.menu = Menu(self.master)
+        self.openMenu = Menu(self.menu)
+        self.openMenu.add_command(label='Open')
         # button for loading data
-        self.openButton = Button(self.master, text = "Open", command = utils.open)
-        self.opneButton.pack(in_=self.controls, side="left")
-        # button for going to next data file
-        self.nextButton = Button(self.master, text = "Next", command = utils.next_file)
-        self.nextButton.pack(in_=self.controls, side="left")
-        # button for saving
-        self.saveButton = Button(text = "Save", command = utils.savePick)
-        self.saveButton.pack(in_=self.controls, side="left")
-        # button for basemap display
-        self.basemapButton = Button(text = "Map", command = basemap.basemap)
-        self.basemapButton.pack(in_=self.controls, side="left")
-        # button for help message
-        self.instButton = Button(self.master, text = "Help", command = utils.help)
-        self.instButton.pack(in_=self.controls, side="left")
-        # button for exit
-        self.exitButton = Button(text = "Exit", fg = "red", command = self.close_window)
-        self.exitButton.pack(in_=self.controls, side="left")
-        # button for picking initiation
-        self.pickButton = Button(text = "Pick", fg = "green", command = self.picking)
-        self.pickButton.pack(in_=self.pickControls, side="left")
-        # button for trace view
-        self.traceButton = Button(text = "Trace View", command = None)
-        self.traceButton.pack(in_=self.pickControls, side="left")
-        # button for pick optimization
-        self.pickOptButton = Button(text = "Pick Optimization", command = None)
-        self.pickOptButton.pack(in_=self.pickControls, side="left")
-        # button to toggle on radargram
-        self.radarButton = Button(text = "radar", command = self.show_radar, relief="sunken")
-        self.radarButton.pack(in_=self.switchIm, side="left")        
-        # button to toggle on clutter
-        self.clutterButton = Button(text = "clutter", command = self.show_clutter)
-        self.clutterButton.pack(in_=self.switchIm, side="left")        
-        # call information messageboxs
-        # utils.help()
-        # register click and key events
-        self.key = self.fig.canvas.mpl_connect("key_press_event", self.onkey)
-        self.click = self.fig.canvas.mpl_connect("button_press_event", self.addseg)
-        self.f_loadName = ""
-        self.dtype = "amp"
-        self.var_reset()
-        utils.open()
+        
+
+
+    #     self.openButton = Button(self.master, text = "Open", command = utils.open)
+    #     self.opneButton.pack(in_=self.controls, side="left")
+    #     # button for going to next data file
+    #     self.nextButton = Button(self.master, text = "Next", command = utils.next_file)
+    #     self.nextButton.pack(in_=self.controls, side="left")
+    #     # button for saving
+    #     self.saveButton = Button(text = "Save", command = utils.savePick)
+    #     self.saveButton.pack(in_=self.controls, side="left")
+    #     # button for basemap display
+    #     self.basemapButton = Button(text = "Map", command = basemap.basemap)
+    #     self.basemapButton.pack(in_=self.controls, side="left")
+    #     # button for help message
+    #     self.instButton = Button(self.master, text = "Help", command = utils.help)
+    #     self.instButton.pack(in_=self.controls, side="left")
+    #     # button for exit
+    #     self.exitButton = Button(text = "Exit", fg = "red", command = self.close_window)
+    #     self.exitButton.pack(in_=self.controls, side="left")
+    #     # button for picking initiation
+    #     self.pickButton = Button(text = "Pick", fg = "green", command = imPick.imPick.picking)
+    #     self.pickButton.pack(in_=self.pickControls, side="left")
+    #     # button for trace view
+    #     self.traceButton = Button(text = "Trace View", command = None)
+    #     self.traceButton.pack(in_=self.pickControls, side="left")
+    #     # button for pick optimization
+    #     self.pickOptButton = Button(text = "Pick Optimization", command = None)
+    #     self.pickOptButton.pack(in_=self.pickControls, side="left")
+    #     # button to toggle on radargram
+    #     self.radarButton = Button(text = "radar", command = imPick.imPick.show_radar, relief="sunken")
+    #     self.radarButton.pack(in_=self.switchIm, side="left")        
+    #     # button to toggle on clutter
+    #     self.clutterButton = Button(text = "clutter", command = imPick.imPick.show_clutter)
+    #     self.clutterButton.pack(in_=self.switchIm, side="left")        
+    #     # call information messageboxs
+    #     # utils.help()
+    #     # register click and key events
+    #     self.key = self.fig.canvas.mpl_connect("key_press_event", imPick.imPick.onkey)
+    #     self.click = self.fig.canvas.mpl_connect("button_press_event", imPick.imPick.addseg)
+    #     self.f_loadName = ""
+    #     self.dtype = "amp"
+    #     self.var_reset()
+    #     utils.open()
     
     
-    def var_reset(self):
-        # variable declarations
-        self.pick_state = 0
-        self.pick_layer = 0
-        self.basemap_state = 0
-        self.pick_dict = {}
-        self.pick_idx = []
-        self.toolbar = None
-        self.pick_loc = None
-        self.data_cmin = None
-        self.data_cmax = None
-        self.clut_cmin = None
-        self.clut_cmax = None
-        self.map_loadName = ""
-        self.f_saveName = ""
-        self.data_imSwitch_flag = ""
-        self.clut_imSwitch_flag = ""
+    # def var_reset(self):
+    #     # variable declarations
+    #     self.pick_state = 0
+    #     self.pick_layer = 0
+    #     self.basemap_state = 0
+    #     self.pick_dict = {}
+    #     self.pick_idx = []
+    #     self.toolbar = None
+    #     self.pick_loc = None
+    #     self.data_cmin = None
+    #     self.data_cmax = None
+    #     self.clut_cmin = None
+    #     self.clut_cmax = None
+    #     self.map_loadName = ""
+    #     self.f_saveName = ""
+    #     self.data_imSwitch_flag = ""
+    #     self.clut_imSwitch_flag = ""
