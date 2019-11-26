fixed thickness calculation on export

utils.py
@@ -24,7 +24,7 @@ def savePick(f_saveName, data, pick_dict):
     data = data
     pick_dict = pick_dict
 
-    v_ice = 3e8/(np.sqrt(3.15)*1e6)   # EM wave veloity in ice - for thickness calculation
+    v_ice = 3e8/(np.sqrt(3.15))   # EM wave veloity in ice - for thickness calculation
     # vars to hold info from pick locations
     lon = []
     lat = []
@@ -44,8 +44,8 @@ def savePick(f_saveName, data, pick_dict):
         twtt_bed.append(pick_dict["layer_" + str(_i)][pick_idx[:]]*1e-6)    # convert back to microseconds
 
         # calculate ice thickness - using twtt_bed and twtt_surf
-        thick.append((((pick_dict["layer_" + str(_i)][pick_idx]) - (data["twtt_surf"][pick_idx])) * v_ice) / 2)
-        
+        thick.append(((((pick_dict["layer_" + str(_i)][pick_idx]*1e-6) - (data["twtt_surf"][pick_idx])) * v_ice) / 2))
+
     # combine the data into a matrix for export
     dstack = np.column_stack((np.hstack(lon).T,np.hstack(lat).T,np.hstack(elev_air).T,np.hstack(twtt_surf).T,np.hstack(twtt_bed).T,np.hstack(thick).T))
 