514e4c02b644cffc50d41275a6712fd2dfd65eac
btobers <btober@arizona.edu>
2020-01-07 14:39:21 -0700

    wvPick trace step controls

--- wvPick.py
+++ wvPick.py
@@ -16,13 +16,13 @@ class wvPick(tk.Frame):
         # set up variables
         self.winSize = tk.IntVar(value=20)
         self.stepSize = tk.IntVar(value=50)
-        self.layerVar = tk.IntVar()
-        self.layerVar.trace('w', self.plot_wv)
-        self.trace = 0
+        self.segmentVar = tk.IntVar()
+        self.segmentVar.trace('w', self.plot_wv)
+        self.trace = 0.
         self.pick_dict = None
         self.rePick = None
-        self.rePick_idx = {}        # dictionary of indeces of repicked traces for each layer
-        self.pick_idx0 = []         # list of start index for each pick layer
+        self.rePick_idx = {}        # dictionary of indeces of repicked traces for each segment
+        self.pick_idx0 = []         # list of start index for each pick segment
 
         # set up frames
         infoFrame = tk.Frame(self.parent)
@@ -45,11 +45,11 @@ class wvPick(tk.Frame):
         tk.Label(infoFrame, text="\t").pack(side="left")
         interpButton = tk.Button(infoFrame, text="AutoPick", command=self.autoPick, pady=0).pack(side="left")
         
-        self.layers=[0]
-        self.layerMenu = tk.OptionMenu(infoFrame, self.layerVar, *self.layers)
-        self.layerMenu.pack(side="right",pady=0)
-        self.layerMenu["highlightthickness"]=0
-        layerLabel = tk.Label(infoFrame, text = "pick layer").pack(side="right")
+        self.segments=[0]
+        self.segmentMenu = tk.OptionMenu(infoFrame, self.segmentVar, *self.segments)
+        self.segmentMenu.pack(side="right",pady=0)
+        self.segmentMenu["highlightthickness"]=0
+        segmentLabel = tk.Label(infoFrame, text = "subsurface pick segment").pack(side="right")
 
         # create figure object and datacanvas from it
         self.fig = mpl.figure.Figure()
@@ -57,7 +57,6 @@ class wvPick(tk.Frame):
         self.dataCanvas = FigureCanvasTkAgg(self.fig, self.parent)
         self.dataCanvas.get_tk_widget().pack(in_=self.dataFrame, side="bottom", fill="both", expand=1) 
         self.click = self.fig.canvas.mpl_connect("button_press_event", self.manualPick)
-
         
         # add toolbar to plot
         self.toolbar = NavigationToolbar2Tk(self.dataCanvas, toolbarFrame)
@@ -83,26 +82,38 @@ class wvPick(tk.Frame):
         self.sampleTime = np.arange(0,self.num_sample+1)*self.dt
 
 
-    # set_pickDict is a method which holds the picked layer data for optimization
+    # set_pickDict is a method which holds the picked segment data for optimization
     def set_pickDict(self, pickDict):
         self.pick_dict = pickDict
-        # determine number of pick layers
+        # determine number of pick segments
         self.num_pkLyrs = len(self.pick_dict)
         self.ax.set_visible(True)
         self.update_option_menu()
+
+        # create lists of first and last picked trace number for each segment
+        self.segment_trace_first = []
+        self.segment_trace_last = []
+        for _i in range(self.num_pkLyrs):
+            picked_traces = np.where(self.pick_dict["segment_" + str(_i)] != -1.)[0]
+            self.segment_trace_first.append(picked_traces[0])
+            self.segment_trace_last.append(picked_traces[-1])
+        self.traceNum = np.where(self.pick_dict["segment_" + str(self.segmentVar.get())] != -1.)[0][0]
 
 
     # plot_wv is a method to draw the waveform on the datacanvas
     def plot_wv(self, *args):
         self.ax.clear()
-        self.layer = str(self.layerVar.get())
+        # self.segment = str(self.segmentVar.get())
 
-        traceNum = np.where(self.pick_dict["layer_" + self.layer] != -1.)[0][self.trace]
+        # print(np.where(self.pick_dict["segment_" + self.segment] != -1.))
+        # print(np.where(self.pick_dict["segment_" + self.segment] != -1.)[0])
+        # traceNum = np.where(self.pick_dict["segment_" + self.segment] != -1.)[0][self.segment_trace]
+        # print(traceNum)
 
         # get sample index of pick for given trace
-        pick_idx = utils.find_nearest(self.sampleTime, (self.pick_dict["layer_" + self.layer][traceNum]*1e-6))
+        pick_idx = utils.find_nearest(self.sampleTime, (self.pick_dict["segment_" + str(self.segmentVar.get())][self.traceNum]*1e-6))
 
-        self.ax.plot(self.data_dB[:,traceNum])
+        self.ax.plot(self.data_dB[:,self.traceNum])
         self.ax.axvline(x = pick_idx, color='r')
 
         self.ax.axis(xmin=int(pick_idx-100),xmax=int(pick_idx+100))
@@ -111,24 +122,26 @@ class wvPick(tk.Frame):
 
     # step forward is a method to move backwards by the number of traces entered to stepSize
     def stepBackward(self):
-        self.trace -= self.stepSize.get()
-        self.plot_wv()
+        if self.traceNum - self.stepSize.get() >= self.segment_trace_first[self.segmentVar.get()]:
+            self.traceNum -= self.stepSize.get()
+            self.plot_wv()
 
     # step forward is a method to move forward by the number of traces entered to stepSize
     def stepForward(self):
-        self.trace += self.stepSize.get()
-        self.plot_wv()
+        if self.traceNum + self.stepSize.get() <= self.segment_trace_last[self.segmentVar.get()]:
+            self.traceNum += self.stepSize.get()
+            self.plot_wv()
 
 
-    # update the pick layer menu based on how many layers exist
+    # update the pick segment menu based on how many segments exist
     def update_option_menu(self):
-            menu = self.layerMenu["menu"]
+            menu = self.segmentMenu["menu"]
             menu.delete(0, "end")
             for _i in range(self.num_pkLyrs):
                 menu.add_command(label=_i,
-                    command=tk._setit(self.layerVar,_i))
-                self.rePick_idx["layer_" + str(_i)] = []
-                self.pick_idx0.append(np.where(self.pick_dict["layer_" + str(_i)] != -1.)[0][0])
+                    command=tk._setit(self.segmentVar,_i))
+                self.rePick_idx["segment_" + str(_i)] = []
+                self.pick_idx0.append(np.where(self.pick_dict["segment_" + str(_i)] != -1.)[0][0])
 
 
     def autoPick(self):
@@ -144,21 +157,21 @@ class wvPick(tk.Frame):
         self.dataCanvas.draw()
 
         # append trace number to rePick_idx list to keep track of indeces for interpolation
-        if (len(self.rePick_idx["layer_" + str(self.layerVar.get())]) == 0) or (self.rePick_idx["layer_" + str(self.layerVar.get())][-1] != self.trace):
-            self.rePick_idx["layer_" + str(self.layerVar.get())].append(self.trace)
+        if (len(self.rePick_idx["segment_" + str(self.segmentVar.get())]) == 0) or (self.rePick_idx["segment_" + str(self.segmentVar.get())][-1] != self.segment_trace):
+            self.rePick_idx["segment_" + str(self.segmentVar.get())].append(self.traceNum)
 
-        self.pick_dict["layer_" + self.layer][self.pick_idx0[self.layerVar.get()] + self.trace] = round(event.xdata)*self.dt*1e6
+        self.pick_dict["segment_" + self.segment][self.pick_idx0[self.segmentVar.get()] + self.traceNum] = round(event.xdata)*self.dt*1e6
 
 
     # interpPicks is a method to interpolate linearly between refined picks
     def interpPicks(self):
         for _i in range(len(self.rePick_idx)):
-            if len(self.rePick_idx["layer_" + str(_i)]) >= 2:
-                # get indices where picks exist for pick layer
-                x = np.where(self.pick_dict["layer_" + str(_i)] != -1)[0]
+            if len(self.rePick_idx["segment_" + str(_i)]) >= 2:
+                # get indices where picks exist for pick segment
+                x = np.where(self.pick_dict["segment_" + str(_i)] != -1)[0]
                 # get indeces of repicked traces
-                xp = self.pick_idx0[_i] + self.rePick_idx["layer_" + str(_i)]
+                xp = self.pick_idx0[_i] + self.rePick_idx["segment_" + str(_i)]
                 # get twtt values at repicked indices
-                fp = self.pick_dict["layer_" + str(_i)][xp]
-                # interpolate repicked values for layer
-                self.pick_dict["layer_" + str(_i)][x] = np.interp(x, xp, fp)                 
+                fp = self.pick_dict["segment_" + str(_i)][xp]
+                # interpolate repicked values for segment
+                self.pick_dict["segment_" + str(_i)][x] = np.interp(x, xp, fp)                 
