c90fa247c8aa3be780cf00f0cc1f0fb8febf7723
btobers <tobers.brandon@gmail.com>
2021-10-18 22:03:01 -0700

    sharad ingest cleanup

--- code/ingest/ingest_sharad.py
+++ code/ingest/ingest_sharad.py
@@ -20,6 +20,7 @@ def read(fpath, simpath, navcrs, body):
     rdata.fn = fpath.split("/")[-1][:-10]
     rdata.dtype = "sharad"
     root = os.path.dirname(fpath)
+
     print("----------------------------------------")
     print("Loading: " + rdata.fn)
     # rdata.fn = fn.rstrip("_rgram.img")
